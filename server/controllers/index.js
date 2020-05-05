933bfb8d32012dbd2e184abb107042aa0b66ef3b
Faisal Amin <faisal.nstu@ymail.com>
2020-05-05 15:36:07 +0600

    todo  include items

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+const todos = require('./todo');
+const todoItems = require('./todoitems');
+
+module.exports = {
+  todos,
+  todoItems
+};
---
+++ server/controllers/todo.js
@@ -0,0 +1,24 @@
+const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
+
+module.exports = {
+  create(req, res) {
+    return Todo
+      .create({
+        title: req.body.title,
+      })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return Todo
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  }
+};
