ccb3071e89a25b7c853a9ae2e1e54d79b246cf3b
Faisal Amin <faisal.nstu@ymail.com>
2020-05-05 16:03:57 +0600

    update, delete apis

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -10,6 +10,7 @@ module.exports = {
       .then(todo => res.status(201).send(todo))
       .catch(error => res.status(400).send(error));
   },
+
   list(req, res) {
     return Todo
       .findAll({
@@ -19,6 +20,66 @@ module.exports = {
         }],
       })
       .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return Todo
+      .findByPk(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Todo
+      .findByPk(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo))  // Send back the updated todo.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Todo
+      .findByPk(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(400).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .destroy()
+          .then(() => res.status(200).send({ message: 'Todo deleted successfully.' }))
+          .catch(error => res.status(400).send(error));
+      })
       .catch(error => res.status(400).send(error));
   }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,4 +11,10 @@ module.exports = (app) => {
   app.post('/api/todos/:todoId/items', todoItemsController.create);
   
   app.get('/api/todos', todosController.list);
+  
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  
+  app.put('/api/todos/:todoId', todosController.update);
+  
+  app.delete('/api/todos/:todoId', todosController.destroy);
 };
